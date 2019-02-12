857281a246374f83fc5db11ad1f69840b016e7dd
MatthewBurstein <matthew.burstein@softwire.com>
2019-02-12 11:58:19 +0000

    MSPSDS-949 feat: add multiple hazards in testing

--- mspsds-web/app/assets/application/javascripts/investigations/ts_investigations/unsafe.js
+++ mspsds-web/app/assets/application/javascripts/investigations/ts_investigations/unsafe.js
@@ -2,26 +2,25 @@ import $ from 'jquery';
 import {simpleAccessibleAutocomplete} from "../../autocomplete";
 
 $(document).ready(() => {
-    const addDeleteFunctionality = link => {
-        link.addEventListener('click', e => {
-            e.preventDefault();
-            $(e.target).parent().parent().remove()
-        })
-    };
+    $("[id^=hazard-type-picker]").toArray().forEach(e => {
+        simpleAccessibleAutocomplete(e.id, { showAllValues: true });
+    });
 
     const addHazardLink =  $('.add-hazard-link')[0];
-    addDeleteFunctionality($('.delete-hazard-link')[0]);
 
     addHazardLink.addEventListener('click', e => {
-        e.preventDefault();
-        const $allHazardsContainer = $('#hazard-type-auto-complete-group-container');
-        const $newHazardSelect = $('#hazard-type-auto-complete-container').clone(true);
-        addDeleteFunctionality($newHazardSelect.find('a')[0]);
-        $allHazardsContainer.append($newHazardSelect)
-        // const newSelectId = ("hazard-type-picker" + Math.random()).replace(/\./, '');
-        // $newHazardSelect.find('select').attr('id', newSelectId);
-        // $("[id^=hazard-type-picker]")
-        //     .toArray()
-        //     .forEach(elem => simpleAccessibleAutocomplete(elem.id, { showAllValues: true }))
+            e.preventDefault();
+            $('#hazard-type-auto-complete-container.display-none')
+                .first()
+                .removeClass('display-none')
+    });
+
+    $('.delete-hazard-link')
+        .toArray()
+        .forEach(link => {
+            link.addEventListener('click', e => {
+                e.preventDefault();
+                $(e.target).parent().parent().addClass('display-none')
+        })
     });
 });
