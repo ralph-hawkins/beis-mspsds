339219d1dacf02548ccf33caf421eaafef151aa9
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2019-02-21 15:40:53 +0000

    MSPSDS-904: WIP: Only run MSPSDS tests

--- ci/test-if-changed.sh
+++ ci/test-if-changed.sh
@@ -5,7 +5,7 @@ docker-compose -f docker-compose.yml -f docker-compose.ci.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.ci.yml run --rm --no-deps $COMPONENT echo 'Gems pre-installed'
 docker-compose -f docker-compose.yml -f docker-compose.ci.yml up -d $COMPONENT
     
-if [[ $(./ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
+if [[ $COMPONENT = "mspsds-web" ]]; then
     echo "Testing component $COMPONENT"
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml run --rm start_dependencies
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rake db:create db:schema:load test:all
